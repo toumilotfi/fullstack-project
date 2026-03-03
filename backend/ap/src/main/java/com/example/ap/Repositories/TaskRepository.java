96863576ec70fca51b701e958dd983299f71213e
lotfi <lotfitoumi56@gmail.com>
2026-03-04 00:04:26 +0100

    add the repositories for the project

---
+++ backend/ap/src/main/java/com/example/ap/Repositories/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.example.ap.Repositories;
+
+import com.example.ap.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+    List<Task> findByAssignedToUserId(Integer userId);
+}
