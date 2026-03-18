a2fc34091a569c743022ef9264d70a266eda8541
lotfi <lotfitoumi56@gmail.com>
2026-03-18 14:41:10 +0100

    update

--- frontend-web/app/src/main.ts
+++ frontend-web/app/src/main.ts
@@ -3,6 +3,10 @@ import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } 
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { App } from './app/app'; // <--- Notice it imports 'App' from './app/app'
 import { routes } from './app/app.routes';
+import { addIcons } from 'ionicons';
+import * as icons from 'ionicons/icons';
+
+addIcons(icons);
 bootstrapApplication(App, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
